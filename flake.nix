59b40de971cc7a5a543bba24c600e63e817f8eda
dzmitry-lahoda <dzmitry@lahoda.pro>
2024-05-10 22:47:51 +0100

    update nix

--- flake.nix
+++ flake.nix
@@ -2,18 +2,26 @@
   description = "bash-commons via nix flakes";
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        formatter = pkgs.alejandra;
         packages.default = pkgs.stdenvNoCC.mkDerivation rec {
           pname = "bash-commons";
           version = "0.1.9";
           src = ./modules/bash-commons/src;
-          phases = [ "installPhase" "fixupPhase" ];
+          phases = ["installPhase" "fixupPhase"];
           installPhase = ''
             mkdir -p $out/bin
             cp $src/array.sh $out/bin/array.sh
@@ -22,7 +30,7 @@
             cp $src/log.sh $out/bin/log.sh
             cp $src/string.sh $out/bin/string.sh
             cp $src/string.sh $out/bin/os.sh
-
+        
             chmod +x $out/bin/array.sh
             chmod +x $out/bin/assert.sh
             chmod +x $out/bin/file.sh
@@ -34,7 +42,7 @@
           doCheck = false;
         };
         devShells.default = pkgs.mkShell {
-          buildInputs = [ self'.packages.default ];
+          buildInputs = [self'.packages.default];
         };
       };
     };
