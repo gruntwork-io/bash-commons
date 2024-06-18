8ca213d8c5479a943620e95b99738ff56e43f35c
Justin-Octo <116027513+justin-octo@users.noreply.github.com>
2024-06-18 11:13:54 -0700

    wrap curl in variable
    curl command failure cause script to abort. Wrapping inside a variable should allow the script to continue.

--- modules/bash-commons/src/aws.sh
+++ modules/bash-commons/src/aws.sh
@@ -25,16 +25,14 @@ readonly six_hours_in_s=21600
 # to consult by setting the environment variable GRUNTWORK_BASH_COMMONS_IMDS_VERSION
 # Defaults to IMDSv2 since that is now enabled by default on instances (IMDS only has two options,
 # "optional" = both v1 and v2, or "required" = v2 only).  All new instances support v2 now.
-curl -s -o /dev/null -X PUT ${imdsv2_token_endpoint} -H "X-aws-ec2-metadata-token-ttl-seconds: 10"
-finish_code=$(echo $?)
-if [ ${finish_code} -eq 0 ]; then
+to_token_or_not_to_token=$(curl -s -o /dev/null -X PUT ${imdsv2_token_endpoint} -H "X-aws-ec2-metadata-token-ttl-seconds: 10"; echo $?)
+if [ ${to_token_or_not_to_token} -eq 0 ]; then
   default_instance_metadata_version="2"
-elif [ ${finish_code} -eq 7 ]; then
+elif [ ${to_token_or_not_to_token} -eq 7 ]; then
   echo "IMDS endpoint connection refused."
   default_instance_metadata_version="0"
 else
-  curl -s -o /dev/null $metadata_endpoint
-  finish_code=$(echo $?)
+  finish_code=$(curl -s -o /dev/null $metadata_endpoint; echo $?)
   if [ ${finish_code} -eq 0 ]; then
     default_instance_metadata_version="1"
   elif [ ${finish_code} -eq 7 ]; then
